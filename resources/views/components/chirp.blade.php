40dc088e7dad341773c85797fdd915d9989c1d23
andiartori <rizkyandiarto@gmail.com>
2026-07-03 16:49:09 +0700

    mastering/Seeder: progressing

--- resources/views/components/chirp.blade.php
+++ resources/views/components/chirp.blade.php
@@ -1,1 +1,34 @@
-<?php
+@props(['chirp'])
+
+<div class="card bg-base-100 shadow mt-8">
+    <div class="card-body">
+        <div class="flex space-x-3">
+            @if($chirp->user)
+                <div class="a   vatar">
+                    <div class="size-10 rounded-full">
+                        <img src="https://avatars.laravel.cloud/>{{ urlencode($chirp->user->email)}}"
+                        alt="{{$chirp->user->name}}"
+                        class="rounded-full">
+                    </div>
+                </div>
+            @else
+                <div class="avatar placeholder">
+                    <div class="size-10 rounded-full">
+                        <img src="https://avatars.laravel.cloud/f61123d5-0b27-434c-a4ae-c653c7fc9ed6?vibe=stealth"
+                        alt="{{ $chirp->user->name }}"
+                        class="rounded-full">
+                    </div>
+
+                </div>
+            @endif
+
+            <div class="min-w-0">
+                <div class="flex items-center gap-1" >
+                    <span class="semibold" > {{ $chirp->user ? $chirp->user->name : "Anonymous"  }}</span>
+                    <span class="mt-1"> {{ $chirp->messgage  }}</span>
+                    <span class="mt-1"> {{ $chirp->created_at->diffForHumans()  }} </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,20 +9,7 @@
         <p class="mt-4 text-base-content/60">This is your brand new Laravel application. Time to make it
             sing (or chirp)!</p>
         @forelse($chirps as $chirp)
-            <div class="card bg-base-100 shadow mt-8">
-                <div class="card-body">
-                        <div class="semibold">{{$chirp->user ? $chirp->user->name : "Anonymous"}}</div>
-                    <div class="mt-1">
-                        {{ $chirp->message  }}
-                    </div>
-                    <div class="mt-1">
-                        {{ $chirp->created_at->diffForHumans()  }}
-                    </div>
-
-                </div>
-
-            </div>
-
+            <x-chirp :chirp="$chirp"/>
         @empty
         <div> <p>No Chirp for the moment</p> </div>
         @endforelse
