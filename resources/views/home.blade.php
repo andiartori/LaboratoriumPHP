2774500e8ef9e5cf833dfc5973ae8a7564d4c4d9
andiartori <rizkyandiarto@gmail.com>
2026-07-03 15:28:37 +0700

    Phase2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,17 +8,37 @@
         <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
         <p class="mt-4 text-base-content/60">This is your brand new Laravel application. Time to make it
             sing (or chirp)!</p>
-        @foreach($chirps as $chirp)
+        @forelse($chirps as $chirp)
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
-                    <div>
-
-                        <p> {{ $chirp['author'] }}</p>
-                        <p> {{ $chirp['message'] }}</p>
-                        <p> {{ $chirp['time']  }} </p>
+                        <div class="semibold">{{$chirp->user ? $chirp->user->name : "Anonymous"}}</div>
+                    <div class="mt-1">
+                        {{ $chirp->message  }}
                     </div>
+                    <div class="mt-1">
+                        {{ $chirp->created_at->diffForHumans()  }}
+                    </div>
+
                 </div>
+
             </div>
-        @endforeach
+
+        @empty
+        <div> <p>No Chirp for the moment</p> </div>
+        @endforelse
+
+{{--        The Old Code of having a dummy data.--}}
+{{--        @foreach($chirps as $chirp)--}}
+{{--            <div class="card bg-base-100 shadow mt-8">--}}
+{{--                <div class="card-body">--}}
+{{--                    <div>--}}
+
+{{--                        <p> {{ $chirp['author'] }}</p>--}}
+{{--                        <p> {{ $chirp['message'] }}</p>--}}
+{{--                        <p> {{ $chirp['time']  }} </p>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        @endforeach--}}
     </div>
 </x-layout>
