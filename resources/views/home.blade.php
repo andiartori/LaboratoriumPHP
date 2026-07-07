f5a5c522555b9d832ba4e0f5e7b5c1505b8034da
andiartori <rizkyandiarto@gmail.com>
2026-07-07 22:40:17 +0700

    frontEnd Related

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,50 +8,53 @@
         <p class="mt-4 text-base-content/60">This is your brand new Laravel application. Time to make it
             sing (or chirp)!</p>
 
-<div class="card bg-base-100 shadow mt-8">
-    <div class="card-body">
-        <form action="" method="" >
-            @csrf
-         <div class="rounded-lg w-full form-control ">
-            <textarea
-                    name ="message",
-                    placeholder="Input your chirp"
-                    rows= "4",
-                    maxlength = "255",
-                    required,
-                    class="text-area text-area-bordered w-full resize-none"
-                >
-            </textarea>
-         </div>
-         <div class="flex justify-end p-5" >
-          <button class="btn btn-primary btn-sm">
-            Submit
-          </button>
-         </div>
-        </form>
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <form action="/chirps" method="POST">
+                    @csrf
+                    <div class="rounded-lg w-full form-control ">
+                        <textarea name ="message", placeholder="Input your chirp" rows= "4", maxlength = "255", required,
+                            class="text-area text-area-bordered w-full resize-none">
+                         </textarea>
+                    </div>
+
+                    @error('message')
+                        <div class='label'>
+                            <span class='label-text-alt text-error'>{{ $message }}</span>
+                        </div>
+                    @enderror
+
+                    <div class="flex justify-end p-5">
+                        <button type="submit" class="btn btn-primary btn-sm">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
-</div>
 
 
-                @forelse($chirps as $chirp)
-            <x-chirp :chirp="$chirp"/>
+        @forelse($chirps as $chirp)
+            <x-chirp :chirp="$chirp" />
         @empty
 
-        <div> <p>No Chirp for the moment</p> </div>
+            <div>
+                <p>No Chirp for the moment</p>
+            </div>
         @endforelse
 
-{{--        The Old Code of having a dummy data.--}}
-{{--        @foreach($chirps as $chirp)--}}
-{{--            <div class="card bg-base-100 shadow mt-8">--}}
-{{--                <div class="card-body">--}}
-{{--                    <div>--}}
+        {{--        The Old Code of having a dummy data. --}}
+        {{--        @foreach ($chirps as $chirp) --}}
+        {{--            <div class="card bg-base-100 shadow mt-8"> --}}
+        {{--                <div class="card-body"> --}}
+        {{--                    <div> --}}
 
-{{--                        <p> {{ $chirp['author'] }}</p>--}}
-{{--                        <p> {{ $chirp['message'] }}</p>--}}
-{{--                        <p> {{ $chirp['time']  }} </p>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        @endforeach--}}
+        {{--                        <p> {{ $chirp['author'] }}</p> --}}
+        {{--                        <p> {{ $chirp['message'] }}</p> --}}
+        {{--                        <p> {{ $chirp['time']  }} </p> --}}
+        {{--                    </div> --}}
+        {{--                </div> --}}
+        {{--            </div> --}}
+        {{--        @endforeach --}}
     </div>
 </x-layout>
