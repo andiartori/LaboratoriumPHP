4c8ac365ad3480c7aea4ca3e95ba2972c2539e7c
andiartori <rizkyandiarto@gmail.com>
2026-08-04 15:06:20 +0700

    Auth Progress 9

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,21 @@
 <?php
 
+use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirperController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ChirperController::class, 'index']);
+Route::middleware('auth')->group(function () {
+    Route::get('/', [ChirperController::class, 'index']);
 
-Route::post('/chirps', [ChirperController::class, 'store']);
+    Route::post('/chirps', [ChirperController::class, 'store']);
 
-Route::get('/chirps/{chirp}/edit', [ChirperController::class, 'edit']);
-Route::put('/chirps/{chirp}', [ChirperController::class, 'update']);
+    Route::get('/chirps/{chirp}/edit', [ChirperController::class, 'edit']);
+    Route::put('/chirps/{chirp}', [ChirperController::class, 'update']);
 
-Route::delete('/chirps/{chirp}', [ChirperController::class, 'destroy']);
+    Route::delete('/chirps/{chirp}', [ChirperController::class, 'destroy']);
+
+});
+
+// Auth Necessary things
+Route::view('/register', 'auth.register')->middleware('guest')->name('register');
+Route::post('/register', Register::class)->middleware('guest');
